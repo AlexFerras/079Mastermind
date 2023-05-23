8ffba98ff13b3363612ce39eb4deacb95a1e100b
AlexFerras <78160840+AlexFerras@users.noreply.github.com>
2023-05-24 00:01:51 +0200

    version update

--- Mastermind/Plugin.cs
+++ Mastermind/Plugin.cs
@@ -13,7 +13,7 @@ namespace Plugin
     {
 
         public override string Author => "Ferrasick";
-        public override Version Version => new Version(0, 0, 7);
+        public override Version Version => new Version(0, 0, 8);
         public override Version RequiredExiledVersion => new Version(7, 0, 0, 0);
 
         public static Plugin plugin;
